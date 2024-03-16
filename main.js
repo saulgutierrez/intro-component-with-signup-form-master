5b7307caab0fbc4d095799ce85f8a2e6991b3049
Saúl Gutiérrez <62368834+saulgutierrez@users.noreply.github.com>
2024-03-16 11:50:33 -0600

    Fixed javascript validation

--- main.js
+++ main.js
@@ -16,18 +16,18 @@ button.addEventListener('click', function(e) {
         firstName.style.border = "1px solid red";
         firstName.placeholder = "";
         icon1.style.opacity = "100%";
+    } else {
+        firstName.style.border = "1px solid #e5e5e5";
+        icon1.style.opacity = "0%";
     }
 
     if (lastName.value == "") {
         lastName.style.border = "1px solid red";
         lastName.placeholder = "";
         icon2.style.opacity = "100%";
-    }
-
-    if (email.value == "") {
-        email.style.border = "1px solid red";
-        email.placeholder = "";
-        icon3.style.opacity = "100%";
+    } else {
+        lastName.style.border = "1px solid #e5e5e5";
+        icon2.style.opacity = "0%";
     }
 
     if (emailValidation == false && email.value != "") {
@@ -35,12 +35,23 @@ button.addEventListener('click', function(e) {
         email.placeholder = document.querySelector(".email");
         email.style.color = "red";
         icon3.style.opacity = "100%";
+    } else if (email.value == "") {
+        email.style.border = "1px solid red";
+        email.placeholder = "";
+        icon3.style.opacity = "100%";
+    } else {
+        email.style.border = "1px solid #e5e5e5";
+        icon3.style.opacity = "0%";
+        email.style.color = "black";
     }
 
     if (password.value == "") {
         password.style.border = "1px solid red";
         password.placeholder = "";
         icon4.style.opacity = "100%";
+    } else {
+        password.style.border = "1px solid #e5e5e5";
+        icon4.style.opacity = "0%";
     }
 });
 
